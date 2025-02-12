ea84598db2e8011416e5bedf5f3a2b7ffdf5edab
felipeCabesssa <felipesoares100@hotmail.com>
2025-02-12 16:02:43 -0300

    Segundo cenario - cadastrar

--- cypress/e2e/visit.cy.js
+++ cypress/e2e/visit.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
   it('validado', () => {
-    cy.visit('https://adopet-frontend-cypress.vercel.app/cadastro')
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.contains('a', 'Cadastrar').click();
+    cy.get('input[name="nome"]').type('Felipe Henrique Soares');
+    cy.get('input[name="email"]').type('felipehenrique@mail.com');
+    cy.get('input[name="password"]').type('Senh@1234567');
+    cy.get('input[name="confirm_password"]').type('Senh@1234567');
+    cy.contains('button', 'Cadastrar').click();
   })
 })
