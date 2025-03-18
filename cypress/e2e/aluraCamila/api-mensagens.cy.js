8d73d12e0d7b7c8e592d9aae2136e0e6bee1e0f8
felipeCabesssa <felipesoares100@hotmail.com>
2025-03-18 17:30:49 -0300

    Exemploe de teste "flaky" (intermitente)

--- cypress/e2e/aluraCamila/api-mensagens.cy.js
+++ cypress/e2e/aluraCamila/api-mensagens.cy.js
@@ -1,4 +1,5 @@
 describe('Api Adopet', () => {
+    const tempoEsperado = Math.random() * 1000
     const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJmYmFkMzI4MC0zOWE0LTQ1YWMtYjA2Mi1jMjU0ZGZlMDgxMjQiLCJhZG9wdGVyTmFtZSI6ImZlbGlwZWFuZHJhZGUiLCJpYXQiOjE3NDIzMTQ4NDcsImV4cCI6MTc0MjU3NDA0N30.FAjjeh-g_FpUHc8Zqqi9VrS8ZEqxIrEtJja8xQ6vv9k`
 
     it('Mensagens da API', () => {
@@ -11,6 +12,7 @@ describe('Api Adopet', () => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
             expect(res.body).to.have.property('msg')
+            expect(res.duration).to.be.lte(tempoEsperado)
         })
     })
 })
